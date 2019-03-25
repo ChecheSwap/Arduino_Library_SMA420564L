37069c31c9e980a0517c464a3021573564e4053c
Jesús José Navarrete Baca <chechenb19972@hotmail.com>
2019-03-25 00:44:50 -0700

    Add files via upload
    Init Commit

---
+++ lcd_demolib.ino
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include "seven_segslcd.h" //Inclusion de libreria
+
+seven_segslcd * obj = new seven_segslcd(); //Instanciacion de Objeto
+
+void setup()
+{}
+void loop()
+{
+    int numero = 9;
+
+    for(int posicion = 3; posicion >= 0; --posicion){
+
+        while(!(numero < -1)){
+            obj->print_digit(numero,posicion);
+            delay(250);
+            --numero;
+        }
+        numero = 9;
+    }
+}
+
+
+
+
